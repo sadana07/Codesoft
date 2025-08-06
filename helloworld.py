43864e0d2f6c681193596130bccf86d1bc88e477
Sakthi3431 <supersakthi0404@gmail.com>
2025-08-06 04:20:11 -0700

    commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+# num =int(input("enter the no: "))
+# if (num<0):
+#     print("negative,the number is", num)
+# elif (num>0):
+#     print("positive,the number is:", num)
+# elif (num==0):
+#     print("number is 0")
+    
+meghna=input()
+if (meghna=="die"):
+    print("surya meets priya")
+else:
+    print("surya weds meghna")
